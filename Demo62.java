85d0c504469fa2a500cd5cbbe7074f6970d0c0d4
Aryan07 <vallakatiaryan@gmail.com>
2025-02-06 09:46:37 +0530

    Add files via upload

---
+++ Demo62.java
@@ -0,0 +1,13 @@
+public class Demo62 {
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 40, 50};
+        if (numbers.length < 2) {
+            System.out.println("Array should have at least two elements.");
+        } else {
+            System.out.println("First element: " + numbers[0]);
+            System.out.println("Second element: " + numbers[1]);
+            System.out.println("Last element: " + numbers[numbers.length - 1]);
+        }
+    }
+}
+
